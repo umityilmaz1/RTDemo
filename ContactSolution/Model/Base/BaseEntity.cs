51875233dd228b727ae0b0f8bdcf89ed3f5eae45
Ümit Yılmaz <umityilmaz1099@gmail.com>
2023-05-05 16:06:15 +0300

    Api, Core and Model projects added, repository pattern added to core project, base entity added to Model project

---
+++ ContactSolution/Model/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Model.Base
+{
+    public class BaseEntity
+    {
+        public int ID { get; set; }
+        //public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        //public string UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
