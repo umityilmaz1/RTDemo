86624e60a58bd71ea8b9b8f4b2d44c6e7f513640
Ümit Yılmaz <umityilmaz1099@gmail.com>
2023-05-06 20:02:44 +0300

    report api program.cs migration code added

--- ReportSolution/Api/Program.cs
+++ ReportSolution/Api/Program.cs
@@ -1,4 +1,8 @@
 using Api.AutoMapperConfigurations;
+using Microsoft.EntityFrameworkCore;
+using Repository.Context;
+
+const string _defaultConnectionString = "Default";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +16,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<RTDemoContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString(_defaultConnectionString)));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<RTDemoContext>();
+    context.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
